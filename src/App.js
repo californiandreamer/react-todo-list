f83131d0fa19a86d5f8b0d029a5884732aabd8ed
californiandreamer <sydyahanazar777@gmail.com>
2021-02-18 19:52:27 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import s from "./App.module.css";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const initialData = JSON.parse(localStorage.getItem("tasks"));
+    const [values, setValues] = useState("");
+    const [tasksList, setTask] = useState(initialData || []);
+    console.log(tasksList);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setTask([
+            ...tasksList,
+            {
+                id:
+                    tasksList.length === 0
+                        ? 0
+                        : tasksList.indexOf(tasksList.slice(-1)[0]) + 1,
+                task: values,
+            },
+        ]);
+    };
+
+    const saveDataToStorage = () => {
+        localStorage.setItem("tasks", JSON.stringify(tasksList));
+    };
+
+    const removeTask = (e, index) => {
+        console.log(index);
+        e.preventDefault();
+        if (index > -1) {
+            tasksList.splice(index, 1);
+        }
+        saveDataToStorage();
+    };
+
+    const mapped = tasksList.map((val) => {
+        return (
+            <div className={s.card} key={val.id}>
+                <div className={s.card__inner}>
+                    <div className={s.card__content}>
+                        {val.id}. {val.task}
+                    </div>
+                    <button onClick={(e) => removeTask(e, val.id)}>-</button>
+                </div>
+            </div>
+        );
+    });
+
+    useEffect(() => {
+        saveDataToStorage();
+    }, [tasksList]);
+
+    return (
+        <div className="App">
+            <div className={s.form__row} onSubmit={handleSubmit}>
+                <form className={s.form}>
+                    <input
+                        className={s.form__input}
+                        type="text"
+                        vlaue={values}
+                        onChange={(e) => setValues(e.target.value)}
+                        placeholder="Input new task"
+                    />
+                    <button className={s.form__btn}>+</button>
+                </form>
+            </div>
+            <div className="Cards-box">{mapped}</div>
+        </div>
+    );
 }
 
 export default App;
